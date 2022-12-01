bd5b92bbefe1775dc0d35a535cd38fd211c4f462
RainMakerDEVELOP <winekein@naver.com>
2022-12-01 16:44:20 +0900

    2022.12.01 #002
    - cliproc package
     . 모니터링 수집 대상 명칭 목록 생성
     . REST 구조체 추가
     . Run 함수의 메인 루틴 주석 작성
     . makeJsonData 함수 추가
     . reqClient 함수 추가

--- pwcpkg/PcWrnCheckerCliProc_v1.go
+++ pwcpkg/PcWrnCheckerCliProc_v1.go
@@ -5,36 +5,71 @@
 package cliproc
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
 	"strings"
 )
 
+// 모니터링 수집 대상 명칭 목록
 const (
-	USEDCPU = "USEDCPU"
+	USEDCPU = "USEDCPU" // CPU 사용량
+	USEDMEM = "USEDMEM" // Memory 사용량
 )
+
+type RestData struct {
+	ItemName string `json:"itemname"`
+	Value    string `json:"value"`
+}
 
 func Run() bool {
 	fmt.Println("CliProc Run Function Start!")
 
-	// 실행인자 체크 루틴 실행
+	// [PROC] 실행인자 체크 루틴 실행
 	var strType string
 	var bRet bool
 
 	if bRet, strType = readArg(); !bRet {
 		return false
 	}
-
 	fmt.Printf("readArg() ret strType = '%v'\n", strType)
-	// 서버 전송용 JSON 설정
 
-	// 서버 전송
+	// [PROC] 서버 전송용 JSON 설정
+	btSendData, marshalErr := makeJsonData(strType)
+	if marshalErr != nil {
+		log.Panicln(marshalErr.Error())
+		return false
+	}
 
-	// 첫번째 전송 성공하면 goroutine 실행하여 주기적으로 전송
+	if len(btSendData) <= 0 {
+		fmt.Printf("marshaling data return len = '%v'\n", len(btSendData))
+		return false
+	}
+	fmt.Println(btSendData)
+	fmt.Println(string(btSendData))
 
-	// 주기적 전송 루틴이 goroutine 으로 실행되므로, goroutine 종료전에 리턴되면 안되므로 체크하여 기다리는 루틴 삽입
+	// [PROC] 서버 전송
+	resp, bReqRet := reqClient(btSendData)
+	if !bReqRet {
+		fmt.Println("http request FAILED")
+		return false
+	}
 
-	// 모든 하위 프로세스가 종료되면 리턴한다.
+	defer resp.Body.Close()
+
+	// [PROC] 첫번째 전송 성공인지 응답 체크
+	if resp.StatusCode == http.StatusOK {
+		// goroutine 실행하여 주기적으로 전송
+		// resp.Body.Read
+	}
+
+	// [PROC] 주기적 전송 루틴이 goroutine 으로 실행되므로, goroutine 종료전에 리턴되면 안되므로 체크하여 기다리는 루틴 삽입
+
+	// [PROC] 모든 하위 프로세스가 종료되면 리턴한다.
 
 	fmt.Println("CliProc Run Function End!")
 	return true
@@ -49,7 +84,7 @@ func readArg() (bool, string) {
 		}
 	}
 
-	// 필수 실행인자가 없었으면 실패
+	// 필수 실행인자가 없으면 실패
 	if len(arg) <= 0 {
 		fmt.Printf("not enough argument (cnt = '%v')\n", len(arg))
 		return false, ""
@@ -64,4 +99,28 @@ func readArg() (bool, string) {
 	fmt.Printf("'%v' in type list\n", arg[0])
 
 	return true, arg[0]
+}
+
+func makeJsonData(data string) ([]byte, error) {
+	stData := RestData{data, "START"}
+	return json.Marshal(stData)
+}
+
+func reqClient(btSendData []byte) (*http.Response, bool) {
+	client := http.Client{}
+	parsedUrl, _ := url.Parse("http://localhost:1234/USEDCPU")
+	req, errRequest := http.NewRequest(http.MethodPost, parsedUrl.String(), bytes.NewBuffer(btSendData))
+	if errRequest != nil {
+		log.Panicln(errRequest.Error())
+		return nil, false
+	}
+
+	res, err := client.Do(req)
+	if err != nil {
+		log.Panicln(errRequest.Error())
+		return nil, false
+	}
+	fmt.Printf("http response status = '%v'\n", res.Status)
+
+	return res, true
 }
